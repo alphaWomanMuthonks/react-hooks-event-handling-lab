a160b96cbd04fb83c4fa6ebe8db4bcd034b8e713
Joy Muthoni Malanga <joyalphamuthoni@gmail.com>
2022-09-26 09:44:51 +0300

    creating an event and handling it

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,19 @@
 // Code Keypad Component Here
+import React from "react"
+
+function handleChange(){
+    console.log('Entering password...');
+}
 
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <input
+            type='password'
+            name='password'
+            placeholder='Enter a password'
+            onChange={handleChange}/>
+        </div>
     )
 }
 
